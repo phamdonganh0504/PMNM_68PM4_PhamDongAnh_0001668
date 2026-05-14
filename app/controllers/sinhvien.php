4e913adb547046270f04165a2bbb8437e431e88d
phamdonganh0504 <phamdonganh180504@gmail.com>
2026-05-14 23:31:11 +0700

    Khoi tao project MVC

---
+++ app/controllers/sinhvien.php
@@ -0,0 +1,12 @@
+<?php
+class sinhvien {
+
+    public function index() {
+        echo "Day la danh sach sinh vien";
+    }
+
+    public function create() {
+        echo "Day la trang tao moi sinh vien";
+    }
+}
+?>
